8e948e66459dba449cba6ccb3f6ecc50a7f21bc7
pos <pgousdal@users.noreply.github.com>
2026-09-22 14:39:11 +0200

    test: allow missing userfile during IRC qualification

--- scripts/irc-integration-test.sh
+++ scripts/irc-integration-test.sh
@@ -20,7 +20,6 @@ set cmdchar .
 join #energymech-ci
 set pub 1
 EOF
-: >"$TMP/data/mech.passwd"
 chmod 600 "$TMP/data/energymech.conf" "$TMP/data/mech.passwd"
 docker network create "$NET" >/dev/null
 docker run -d --name "$IRCD" --network "$NET" ghcr.io/ergochat/ergo:stable >/dev/null
