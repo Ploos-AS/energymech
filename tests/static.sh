cefd0191a04403391cbb7faf5bb2738de311b56f
pos <pgousdal@users.noreply.github.com>
2026-09-23 19:01:34 +0200

    M2.1: validate example configuration

--- tests/static.sh
+++ tests/static.sh
@@ -10,3 +10,11 @@ grep -q 'USER 1000:1000' Dockerfile
 grep -q 'VOLUME \["/data"\]' Dockerfile
 grep -q 'HEALTHCHECK' Dockerfile
 grep -q 'ghcr.io/ploos-as/energymech' compose.yaml
+
+test -s examples/energymech.conf.example
+test -s examples/mech.passwd.example
+grep -q '^set servergroup ' examples/energymech.conf.example
+grep -q '^server ' examples/energymech.conf.example
+grep -q '^nick ' examples/energymech.conf.example
+grep -q '^set userfile mech.passwd$' examples/energymech.conf.example
+! grep -Eq 'pass[[:space:]]+[^R#]' examples/mech.passwd.example
